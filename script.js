3dcbd3bc976db151dfbed3b079b86012b67c341a
DevanshTomar <tomardevansh83@gmail.com>
2023-06-19 12:28:20 -0700

    assign on click listeners to each button

--- script.js
+++ script.js
@@ -5,4 +5,27 @@ $("*").keypress(() => {
     title.text("level " + level);
 })
 
- 
+//assiging the listeners to the buttons.
+//green
+$("#green").click(()=>{
+    let sound = new Audio("./sounds/green.mp3");
+    sound.play();
+})
+
+//red 
+$("#red").click(()=>{
+    let sound = new Audio("./sounds/red.mp3");
+    sound.play();
+})
+
+//yellow
+$("#yellow").click(()=>{
+    let sound = new Audio("./sounds/yellow.mp3");
+    sound.play();
+})
+
+//blue
+$("#blue").click(()=>{
+    let sound = new Audio("./sounds/blue.mp3");
+    sound.play();
+})
